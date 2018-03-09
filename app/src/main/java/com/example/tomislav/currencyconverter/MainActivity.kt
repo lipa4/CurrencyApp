5d47ca73f20c005c40b5ad32ede93f1ee83afef1
Tomislav <tomislav.lipovac@gmail.com>
2018-03-09 21:12:16 +0100

    Create calculator layout

--- app/src/main/java/com/example/tomislav/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/tomislav/currencyconverter/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.tomislav.currencyconverter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.view.ContextThemeWrapper
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setStatusBarTranslucent(true)
     }
+
+    protected fun setStatusBarTranslucent(makeTranslucent: Boolean) {
+        if (makeTranslucent) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        } else {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        }
+    }
+
 }
